41713d3c63e0f0c5017f4a0188ee2bd29acfb6a0
Avanish Pandey <avanishpandey283@gmail.com>
2024-10-03 15:10:45 +0530

    Modified index.js, package-lock.json, and src/createDatabase.js

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-require('dotenv').config()
-const express = require('express')
-const app = require('./src/app')
-const mongoose = require('mongoose')
-const port = process.env.PORT || 3000
+require("dotenv").config();
+const express = require("express");
+const app = require("./src/app");
+const mongoose = require("mongoose");
+
+const port = process.env.PORT || 3003;
+const DATABASE_URL =
+  process.env.MONGO_URI || "mongodb://127.0.0.1:27017/subscribers";
 
 // Parse JSON bodies (as sent by API clients)
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Connect to DATABASE
-// const DATABASE_URL = "mongodb://127.0.0.1:27017/subscribers";
+mongoose.connect(DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-//Connect to Cloud DATABASE (MongoDB Database) 
-const DATABASE_URL = process.env.MONGO_URI
-
-mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection
-db.on('error', (err) => console.log(err))
-db.once('open', () => console.log('connected to database'))
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err));
+db.once("open", () => console.log("Connected to database"));
 
 // Start Server
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`));
